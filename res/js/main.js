625325b71b90f8973784410cbb48a54143e2f3ad
truckdrivercz <eda.orvos@seznam.cz>
2024-10-16 08:02:11 +0200

    update

--- res/js/main.js
+++ res/js/main.js
@@ -3,20 +3,21 @@ const secondStone = document.getElementById("stone1");
 const thirdStone = document.getElementById("stone2");
 const fourthStone = document.getElementById("stone3");
 const fifthStone = document.getElementById("stone4");
+const start = document.getElementById("start");
 let Random;
 
-window.onload = () => {
+start.onclick = () => {
     setInterval(() => {
         for (let i = 0; i < 5; i++)
         {
             Random = Math.random();
             let stone = document.getElementById("stone"+i);
-            let timeValue = Random*2000;
+            let timeValue = Random*1000;
             setTimeout(() => {
                 Random = Math.random();
                 let leftValue = Random*1000;
                 stone.style.left = leftValue+"px";
             }, timeValue);
         }
-    }, 1000);
+    }, 2000);
 }
